16d0efeb37ac8bd63ec063840513bc4ddc9872d1
DRN WINDOWS WSL <desmond.ngueguin@gmail.com>
2020-07-16 06:41:57 +0200

    1er tantative, echouee

--- src/exporter.cpp
+++ src/exporter.cpp
@@ -158,78 +158,138 @@ void Exporter::write_dataframe(std::string file_name, std::string mode){
     if(!file)
         throw string ("ERREUR: Erreur d'ouverture du fichier '" + file_name + "'");
 
-    Solver s = *solver;
+    // Solver s = *solver;
 
     //**************************** En mode trunc, il faut rajouter l'en tete
     if (mode.compare("append") != 0)
         file << "x_min,x_max,y_min,y_max,N,M,c,a,C_v,CFL,precision,t_0,t_f,step_count,rho_expr,sigma_a_expr,sigma_c_expr,E_0_expr,F_0_x_expr,F_0_y_expr,T_0_expr,E_u_expr,F_u_x_expr,F_u_y_expr,T_u_expr,E_d_expr,F_d_x_expr,F_d_y_expr,T_d_expr,E_l_expr,F_l_x_expr,F_l_y_expr,T_l_expr,E_r_expr,F_r_x_expr,F_r_y_expr,T_r_expr,rho_attr,rho,E_f,F_f_x,F_f_y,T_f,E_u,F_u,T_u,E_d,F_d,T_d,E_l,F_l,T_l,E_r,F_r,T_r" << "\n";
 
+    // //**************************** Parametres du probleme
+    // file << s.mesh->x_min << "," << s.mesh->x_max << "," << s.mesh->y_min << "," << s.mesh->y_max << "," << s.mesh->N << "," << s.mesh->M << "," << s.c << "," << s.a << "," << s.C_v << ","<< s.CFL << "," << s.precision << "," << s.t_0 << "," << s.t_f << "," << s.step_count << ",\"" << s.rho_expr << "\",\"" << s.sigma_a_expr << "\",\"" << s.sigma_c_expr << "\",\"" << s.E_0_expr << "\",\"" << s.F_0_x_expr << "\",\"" << s.F_0_y_expr << "\",\"" << s.T_0_expr << "\",\"" << s.E_u_expr << "\",\"" << s.F_u_x_expr << "\",\"" << s.F_u_y_expr << "\",\"" << s.T_u_expr << "\",\"" << s.E_d_expr << "\",\"" << s.F_d_x_expr << "\",\"" << s.F_d_y_expr << "\",\"" << s.T_d_expr << "\",\"" << s.E_l_expr << "\",\"" << s.F_l_x_expr << "\",\"" << s.F_l_y_expr << "\",\"" << s.T_l_expr << "\",\"" << s.E_r_expr << "\",\"" << s.F_r_x_expr << "\",\"" << s.F_r_y_expr << "\",\"" << s.T_r_expr << "\",";
+
+    // //************************** Attributs du crenau de densite
+    // file << "\"[";
+    // for (int l = 0; l < s.n_niche; l++){
+    //     if (l != 0) file << ", ";
+    //     file << "(" << s.attr[l][0] << ", " << s.attr[l][1] << ", " << s.attr[l][2] << ", " << s.attr[l][3] << ")";
+    // }
+    // file << "]\",";
+
+    // //************************** Densite
+    // file << "\"[";
+    // for (int j = 1; j <= s.mesh->M; j++){
+    //     file << "[";
+    //     for (int i = 1; i <= s.mesh->N; i++){
+    //         file << s.rho(s.mesh->x[i], s.mesh->y[j]);
+    //         if (i != s.mesh->N) file << ", ";
+    //     }
+    //     file << "]";
+    //     if (j != s.mesh->M) file << ", ";
+    // }
+    // file << "]\",";
+
+    // write_spatial(file, s.E, s.mesh->N, s.mesh->M);    file << ",";
+
+    // vector<double> F_x(s.mesh->n_cells);
+    // vector<double> F_y(s.mesh->n_cells);
+    // for (int k = 0; k < s.mesh->n_cells; k++){
+    //     F_x[k] = s.F[k][0];
+    //     F_y[k] = s.F[k][1];
+    // }    
+    // write_spatial(file, F_x, s.mesh->N, s.mesh->M);    file << ",";
+    // write_spatial(file, F_y, s.mesh->N, s.mesh->M);    file << ",";
+
+    // write_spatial(file, s.T, s.mesh->N, s.mesh->M);    file << ",";
+
+    // //************************** Signaux sur les bords
+    // // file << "\"[";
+    // // for (int n = 0; n < s.step_count; n++){
+    // //     file << "[";
+    // //     for (int i = 1; i < s.mesh->N+1; i++){
+    // //         // int k = cell_id(i, s.mesh->M, s.mesh->N+2, s.mesh->M+2);
+    // //         file << s.E_up[n][i-1];
+    // //         if (i != s.mesh->N) file << ", ";
+    // //     }
+    // //     file << "]";
+    // //     if (n != s.step_count-1) file << ", ";
+    // // }
+    // // file << "]\",";
+
+    // write_temporal(file, s.E_up, s.step_count, s.mesh->N);    file << ",";
+    // write_temporal(file, s.F_up, s.step_count, s.mesh->N);    file << ",";
+    // write_temporal(file, s.T_up, s.step_count, s.mesh->N);    file << ",";
+
+    // write_temporal(file, s.E_down, s.step_count, s.mesh->N);    file << ",";
+    // write_temporal(file, s.F_down, s.step_count, s.mesh->N);    file << ",";
+    // write_temporal(file, s.T_down, s.step_count, s.mesh->N);    file << ",";
+
+    // write_temporal(file, s.E_left, s.step_count, s.mesh->M);    file << ",";
+    // write_temporal(file, s.F_left, s.step_count, s.mesh->M);    file << ",";
+    // write_temporal(file, s.T_left, s.step_count, s.mesh->M);    file << ",";
+
+    // write_temporal(file, s.E_right, s.step_count, s.mesh->M);    file << ",";
+    // write_temporal(file, s.F_right, s.step_count, s.mesh->M);    file << ",";
+    // write_temporal(file, s.T_right, s.step_count, s.mesh->M);    file << "\n";
+
+
+// ---------------------------------------------------------------------- AVEC solver
+
     //**************************** Parametres du probleme
-    file << s.mesh->x_min << "," << s.mesh->x_max << "," << s.mesh->y_min << "," << s.mesh->y_max << "," << s.mesh->N << "," << s.mesh->M << "," << s.c << "," << s.a << "," << s.C_v << ","<< s.CFL << "," << s.precision << "," << s.t_0 << "," << s.t_f << "," << s.step_count << ",\"" << s.rho_expr << "\",\"" << s.sigma_a_expr << "\",\"" << s.sigma_c_expr << "\",\"" << s.E_0_expr << "\",\"" << s.F_0_x_expr << "\",\"" << s.F_0_y_expr << "\",\"" << s.T_0_expr << "\",\"" << s.E_u_expr << "\",\"" << s.F_u_x_expr << "\",\"" << s.F_u_y_expr << "\",\"" << s.T_u_expr << "\",\"" << s.E_d_expr << "\",\"" << s.F_d_x_expr << "\",\"" << s.F_d_y_expr << "\",\"" << s.T_d_expr << "\",\"" << s.E_l_expr << "\",\"" << s.F_l_x_expr << "\",\"" << s.F_l_y_expr << "\",\"" << s.T_l_expr << "\",\"" << s.E_r_expr << "\",\"" << s.F_r_x_expr << "\",\"" << s.F_r_y_expr << "\",\"" << s.T_r_expr << "\",";
+    file << solver->mesh->x_min << "," << solver->mesh->x_max << "," << solver->mesh->y_min << "," << solver->mesh->y_max << "," << solver->mesh->N << "," << solver->mesh->M << "," << solver->c << "," << solver->a << "," << solver->C_v << ","<< solver->CFL << "," << solver->precision << "," << solver->t_0 << "," << solver->t_f << "," << solver->step_count << ",\"" << solver->rho_expr << "\",\"" << solver->sigma_a_expr << "\",\"" << solver->sigma_c_expr << "\",\"" << solver->E_0_expr << "\",\"" << solver->F_0_x_expr << "\",\"" << solver->F_0_y_expr << "\",\"" << solver->T_0_expr << "\",\"" << solver->E_u_expr << "\",\"" << solver->F_u_x_expr << "\",\"" << solver->F_u_y_expr << "\",\"" << solver->T_u_expr << "\",\"" << solver->E_d_expr << "\",\"" << solver->F_d_x_expr << "\",\"" << solver->F_d_y_expr << "\",\"" << solver->T_d_expr << "\",\"" << solver->E_l_expr << "\",\"" << solver->F_l_x_expr << "\",\"" << solver->F_l_y_expr << "\",\"" << solver->T_l_expr << "\",\"" << solver->E_r_expr << "\",\"" << solver->F_r_x_expr << "\",\"" << solver->F_r_y_expr << "\",\"" << solver->T_r_expr << "\",";
 
     //************************** Attributs du crenau de densite
     file << "\"[";
-    for (int l = 0; l < s.n_niche; l++){
+    for (int l = 0; l < solver->n_niche; l++){
         if (l != 0) file << ", ";
-        file << "(" << s.attr[l][0] << ", " << s.attr[l][1] << ", " << s.attr[l][2] << ", " << s.attr[l][3] << ")";
+        file << "(" << solver->attr[l][0] << ", " << solver->attr[l][1] << ", " << solver->attr[l][2] << ", " << solver->attr[l][3] << ")";
     }
     file << "]\",";
 
     //************************** Densite
     file << "\"[";
-    for (int j = 1; j <= s.mesh->M; j++){
+    for (int j = 1; j <= solver->mesh->M; j++){
         file << "[";
-        for (int i = 1; i <= s.mesh->N; i++){
-            file << s.rho(s.mesh->x[i], s.mesh->y[j]);
-            if (i != s.mesh->N) file << ", ";
+        for (int i = 1; i <= solver->mesh->N; i++){
+            file << (*solver).rho(solver->mesh->x[i], solver->mesh->y[j]);
+            if (i != solver->mesh->N) file << ", ";
         }
         file << "]";
-        if (j != s.mesh->M) file << ", ";
+        if (j != solver->mesh->M) file << ", ";
     }
     file << "]\",";
 
-    write_spatial(file, s.E, s.mesh->N, s.mesh->M);    file << ",";
+    /* Donnes spatialles */
+    write_spatial(file, solver->E, solver->mesh->N, solver->mesh->M);    file << ",";
 
-    vector<double> F_x(s.mesh->n_cells);
-    vector<double> F_y(s.mesh->n_cells);
-    for (int k = 0; k < s.mesh->n_cells; k++){
-        F_x[k] = s.F[k][0];
-        F_y[k] = s.F[k][1];
+    vector<double> F_x(solver->mesh->n_cells);
+    vector<double> F_y(solver->mesh->n_cells);
+    for (int k = 0; k < solver->mesh->n_cells; k++){
+        F_x[k] = solver->F[k][0];
+        F_y[k] = solver->F[k][1];
     }    
-    write_spatial(file, F_x, s.mesh->N, s.mesh->M);    file << ",";
-    write_spatial(file, F_y, s.mesh->N, s.mesh->M);    file << ",";
+    write_spatial(file, F_x, solver->mesh->N, solver->mesh->M);    file << ",";
+    write_spatial(file, F_y, solver->mesh->N, solver->mesh->M);    file << ",";
 
-    write_spatial(file, s.T, s.mesh->N, s.mesh->M);    file << ",";
+    write_spatial(file, solver->T, solver->mesh->N, solver->mesh->M);    file << ",";
 
-    //************************** Signaux sur les bords
-    // file << "\"[";
-    // for (int n = 0; n < s.step_count; n++){
-    //     file << "[";
-    //     for (int i = 1; i < s.mesh->N+1; i++){
-    //         // int k = cell_id(i, s.mesh->M, s.mesh->N+2, s.mesh->M+2);
-    //         file << s.E_up[n][i-1];
-    //         if (i != s.mesh->N) file << ", ";
-    //     }
-    //     file << "]";
-    //     if (n != s.step_count-1) file << ", ";
-    // }
-    // file << "]\",";
+    /* Donnees temporelles */
+    write_temporal(file, solver->E_up, solver->step_count, solver->mesh->N);    file << ",";
+    write_temporal(file, solver->F_up, solver->step_count, solver->mesh->N);    file << ",";
+    write_temporal(file, solver->T_up, solver->step_count, solver->mesh->N);    file << ",";
 
-    write_temporal(file, s.E_up, s.step_count, s.mesh->N);    file << ",";
-    write_temporal(file, s.F_up, s.step_count, s.mesh->N);    file << ",";
-    write_temporal(file, s.T_up, s.step_count, s.mesh->N);    file << ",";
+    write_temporal(file, solver->E_down, solver->step_count, solver->mesh->N);    file << ",";
+    write_temporal(file, solver->F_down, solver->step_count, solver->mesh->N);    file << ",";
+    write_temporal(file, solver->T_down, solver->step_count, solver->mesh->N);    file << ",";
 
-    write_temporal(file, s.E_down, s.step_count, s.mesh->N);    file << ",";
-    write_temporal(file, s.F_down, s.step_count, s.mesh->N);    file << ",";
-    write_temporal(file, s.T_down, s.step_count, s.mesh->N);    file << ",";
+    write_temporal(file, solver->E_left, solver->step_count, solver->mesh->M);    file << ",";
+    write_temporal(file, solver->F_left, solver->step_count, solver->mesh->M);    file << ",";
+    write_temporal(file, solver->T_left, solver->step_count, solver->mesh->M);    file << ",";
 
-    write_temporal(file, s.E_left, s.step_count, s.mesh->M);    file << ",";
-    write_temporal(file, s.F_left, s.step_count, s.mesh->M);    file << ",";
-    write_temporal(file, s.T_left, s.step_count, s.mesh->M);    file << ",";
+    write_temporal(file, solver->E_right, solver->step_count, solver->mesh->M);    file << ",";
+    write_temporal(file, solver->F_right, solver->step_count, solver->mesh->M);    file << ",";
+    write_temporal(file, solver->T_right, solver->step_count, solver->mesh->M);    file << "\n";
 
-    write_temporal(file, s.E_right, s.step_count, s.mesh->M);    file << ",";
-    write_temporal(file, s.F_right, s.step_count, s.mesh->M);    file << ",";
-    write_temporal(file, s.T_right, s.step_count, s.mesh->M);    file << "\n";
+// ----------------------------------------------------------------------
 
     file.close();
 }
