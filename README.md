727e3347d26f135a87d2e6bf1e889fd29ab66bef
DRN WINDOWS WSL <desmond.ngueguin@gmail.com>
2020-08-05 13:19:06 +0200

    compilation avec Clang 6.0.0

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Résolution du problème de propagation de la lumière par la méthode des volum
 _Les commandes indiquées sont à exécuter à partir du répertoire racine du projet._
 
 ### Compilation
-- Avec Cmake: `cmake --build build`
+- Avec Cmake: `cmake --build build` (utiliser GCC 7.4.0 ou Clang 6.0.0 en cas d'erreurs)
 
 ### Exécution
 - Pour une simple exécution: __`build/transfer src/config/simu.cfg`__ 
---
+++ build/CMakeFiles/transfer.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transfer.dir/src/main.cpp.o"
+  "CMakeFiles/transfer.dir/src/exporter.cpp.o"
+  "CMakeFiles/transfer.dir/src/solver.cpp.o"
+  "CMakeFiles/transfer.dir/src/mesh.cpp.o"
+  "CMakeFiles/transfer.dir/src/config.cpp.o"
+  "CMakeFiles/transfer.dir/thirdparty/muparser/src/muParser.cpp.o"
+  "CMakeFiles/transfer.dir/thirdparty/muparser/src/muParserBase.cpp.o"
+  "CMakeFiles/transfer.dir/thirdparty/muparser/src/muParserBytecode.cpp.o"
+  "CMakeFiles/transfer.dir/thirdparty/muparser/src/muParserCallback.cpp.o"
+  "CMakeFiles/transfer.dir/thirdparty/muparser/src/muParserError.cpp.o"
+  "CMakeFiles/transfer.dir/thirdparty/muparser/src/muParserTokenReader.cpp.o"
+  "transfer.pdb"
+  "transfer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transfer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
